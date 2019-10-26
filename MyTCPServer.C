34b522c9c60b273aa67798e99933320d07cd7d32
ledi64 <56673713+ledi64@users.noreply.github.com>
2019-10-26 14:24:17 +0200

    Add files via upload

---
+++ MyTCPServer.C
@@ -0,0 +1,24 @@
+/*
+ * MyTCPServer.C
+ *
+ *  Created on: 24.10.2019
+ *      Author: ledi64
+ */
+
+#include <cstdio> // standard input and output library
+#include <cstdlib> // this includes functions regarding memory allocation
+#include <cstring> // contains string functions
+#include <cerrno> //It defines macros for reporting and retrieving error conditions through error codes
+#include <ctime> //contains various functions for manipulating date and time
+
+#include <unistd.h> //contains various constants
+#include <sys/types.h> //contains a number of basic derived types that should be used whenever appropriate
+#include <arpa/inet.h> // defines in_addr structure
+#include <sys/socket.h> // for socket creation
+#include <netinet/in.h> //contains constants and structures needed for internet domain addresses
+
+#include "SIMPLESOCKET.H"
+#include "MyTCPServer.H"
+
+using namespace std;
+
