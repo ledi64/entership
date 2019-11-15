e6e84be896ad8e27e8aa1adab18035615e734250
ledi64 <56673713+ledi64@users.noreply.github.com>
2019-11-15 15:47:56 +0100

    Add files via upload

---
+++ DestroyShip.C
@@ -0,0 +1,19 @@
+/*
+ * DestroyShip.C
+ *
+ *  Created on: 14.11.2019
+ *      Author: ledi64
+ */
+
+#include "DestroyShip.H"
+#include "TASK3.H"
+#include "TASK3.C"
+
+#include <string>
+
+using namespace std;
+
+
+
+
+
